66f74668dd392dbfe5226a067c041dfab07accf9
Yuli <yulisetsanzonrosas@gmail.com>
2024-09-04 23:37:12 -0600

    Se ha respondido el ejercicio 2 de la práctica 5

--- practicas/p05/ejercicios.php
+++ practicas/p05/ejercicios.php
@@ -25,5 +25,31 @@
     ?>
     </p>
 
+    <h2>Ejercicio 2</h2>
+    <p>
+    <?php
+        // Ejercicio 2: Mostrar contenido de variables
+        $a = "ManejadorSQL";
+        $b = 'MySQL';
+        $c = &$a;
+        echo "a: $a".'<br>';
+        echo "b: $b".'<br>';
+        echo "c: $c".'<br>';
+        
+        $a = "PHP server";
+        $b = &$a;
+        echo '<br> Después de cambiar';
+        echo '<br>'."a: $a"; 
+        echo '<br>'."b: $b";
+        echo '<br>'."c: $c";
+
+        echo '<br><br>En el segundo bloque de asignaciones, se realiza lo siguiente: 
+            <br>$a se le asigna el valor "PHP server". Ahora $a contiene "PHP server".
+            <br>$b se asigna como una referencia a $a. Esto significa que $b ahora apunta a la misma ubicación en la memoria que $a. Cualquier cambio en $a también se reflejará en $b.
+            <br>Debido a esto, tanto $a como $b tienen el valor "PHP server" después de la asignación.
+            <br>La variable $c sigue siendo una referencia a $a. Como $a ha cambiado su valor a "PHP server", $c también refleja este cambio.';
+    ?>
+    </p>
+
 </body>
 </html>
